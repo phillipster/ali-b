a63891e99e69b51f8a7751f892061db936077bae
felipe <lipe.feldman@gmail.com>
2025-04-23 16:09:06 -0400

    Figured out how to make static delivery

--- demo_app/app.py
+++ demo_app/app.py
@@ -1,15 +1,17 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, send_from_directory
 import mysql.connector
 
 app = Flask(__name__, template_folder="templates", static_folder="static")
 
+
 def get_db_conn():
     return mysql.connector.connect(
-      host="localhost",
-      user="root",        # XAMPP default
-      password="",        # XAMPP default
-      database="albert"
+        host="localhost",
+        user="root",
+        password="",
+        database="albert"
     )
+
 
 @app.route("/api/campus")
 def api_items():
@@ -17,12 +19,15 @@ def api_items():
     cursor = conn.cursor(dictionary=True)
     cursor.execute("SELECT * FROM campus")
     items = cursor.fetchall()
-    cursor.close(); conn.close()
+    cursor.close();
+    conn.close()
     return jsonify(items)
 
+
 @app.route("/")
-def home():
-    return render_template("index.html")
+def index():
+    """Serve the static index.html file."""
+    return send_from_directory('static', 'pages/index.html')
 
 
 @app.route("/campuses")
